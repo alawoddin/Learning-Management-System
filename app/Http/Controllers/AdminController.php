3d336f928b6228825665affdf4608226ab383fc9
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-09-19 23:41:01 +0430

    i add the admin profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,4 +27,12 @@ class AdminController extends Controller
 
         return redirect('/admin/login');
     }
+
+     public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
+    }// End Method
+
 }
